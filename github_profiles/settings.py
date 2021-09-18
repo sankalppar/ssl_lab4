ac80c334c9713e5e1861a4ffb7b2cf7ee10799d9
sankalppar <sankalpparasharblog@gmail.com>
2021-09-18 19:19:43 +0530

    Debugging

--- github_profiles/settings.py
+++ github_profiles/settings.py
@@ -135,10 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/css')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
